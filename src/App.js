00acb18e9203714e4ca0f72202deb4f7673a18d0
chendaole <chendaole@4399inc.com>
2017-01-05 18:27:46 +0800

    clear Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import { NICE, SUPER_NICE } from './colors'
-import Counter from './Counter'
 
 const App = () => (
     <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
+        hello world!
     </div>  
 )
 
